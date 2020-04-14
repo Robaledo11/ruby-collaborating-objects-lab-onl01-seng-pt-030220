d128a1857dc1e5d7065a0d48552b5eaa70a990d9
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-04-14 20:52:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,48 +1,32 @@
 class Artist 
+  
+  attr_accessor :name, :songs
 
-attr_accessor :name, :songs 
-attr_reader :song  
-
-@@all = [ ]
-
-
-def initialize(name)
-  @name = name  
-  @@all << self 
-  @songs = [ ]       
-end
-
-def add_song(s)
-    s.artist = self
-end
-
-def self.all 
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = [ ]
+  end 
+  
+  def self.all
     @@all 
+  end 
+  
+  def add_song(song)
+    @songs << song
+  end 
+  
+  def self.find_or_create_by_name(name)
+    artist = @@all.find { |artist| artist.name == name }
+    artist ? artist : self.new(name)
+  end 
+  
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end 
+  
 end 
-
-def self.find_by_name(name)
-   Artist.all.find do |s|
-    s.name == name 
-   end 
-end 
-
-def self.create_by_name(name)
-    artist  = Artist.new(name)
-    return artist 
-end 
-
-def self.find_or_create_by_name(name)
-     artist = self.find_by_name(name)
-    if artist 
-     return artist
-    else
-        artist = self.create_by_name(name)
-    return artist 
-    end 
-end 
-
- def print_songs 
-    @songs.each do |n| 
-        puts n.name 
-    end    
- end 
