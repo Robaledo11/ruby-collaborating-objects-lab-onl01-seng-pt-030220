c32590189b0e1abff6c7293394746c52af5e1ef4
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-04-14 20:52:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,44 +1,32 @@
-class Song 
+class Artist 
+  
+  attr_accessor :name, :songs
 
-attr_accessor :name, :artist 
-
-@@all = [ ]
-
-def initialize(name)
-    @name = name 
-    @@all << self 
-end    
-
-def name=(na)
-    @name = na
-end 
-
-def artist=(a)
-    @artist = a 
-    @artist.songs << self 
-end 
-
-
-def self.all 
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = [ ]
+  end 
+  
+  def self.all
     @@all 
+  end 
+  
+  def add_song(song)
+    @songs << song
+  end 
+  
+  def self.find_or_create_by_name(name)
+    artist = @@all.find { |artist| artist.name == name }
+    artist ? artist : self.new(name)
+  end 
+  
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end 
+  
 end 
-
-def self.new_by_filename(fn)
-   fn_arr = fn.split(" - ")
-   artist_name = fn_arr[0]
-   song_name = fn_arr[1] 
-    song = Song.new(song_name)  
-    a = Artist.new(artist_name)
-    song.artist = a
-    song
-end 
-
-def artist_name=(ar) #get a string from parse file name 
-    a = Artist.find_or_create_by_name(ar)
-    self.artist = a
-end 
-
-
-
-
-end
